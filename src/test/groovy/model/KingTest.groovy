37d262d023a67e0d89adf51ebd4758c4ce4a5ead
szymon <szymon.pardyka@gmail.com>
2022-12-30 21:40:01 +0100

    Small cosmetic code fixes, additional test case

--- src/test/groovy/model/KingTest.groovy
+++ src/test/groovy/model/KingTest.groovy
@@ -3,19 +3,21 @@ package model
 import spock.lang.Specification
 
 class KingTest extends Specification {
-    def 'should find all possible moves when #description'() {
+
+    def 'should find all possible moves when kings #description'() {
         given:
         def king = new King(coordinates, Color.WHITE, PieceValue.KING, true)
-
 
         when:
         def possibleTakes = king.findPossibleTakes(board)
 
         then:
         possibleTakes.size() == expected
+
         where:
         coordinates           | board       | expected | description
         new Coordinates(1, 1) | new Board() | 8        | ' coordinates are 1,1'
-        new Coordinates(0, 0) | new Board() | 3        | ' coordinates are 1,1'
+        new Coordinates(0, 0) | new Board() | 3        | ' coordinates are 0,0'
+        new Coordinates(0, 1) | new Board() | 5        | ' coordinates are 0,1'
     }
 }
