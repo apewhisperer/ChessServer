ceeeda1f013656acedde084c8e4c02f13b7e378a
spardyka <szymon.pardyka@capgemini.com>
2022-12-31 16:52:53 +0100

    King find possible takes Update

--- src/main/java/model/pieces/King.java
+++ src/main/java/model/pieces/King.java
@@ -1,7 +1,9 @@
 package model.pieces;
 
 import lombok.AllArgsConstructor;
-import model.*;
+import model.Board;
+import model.Coordinates;
+import model.Field;
 import model.enums.Color;
 import model.enums.PieceValue;
 import utils.StreamUtils;
@@ -11,7 +13,7 @@ import java.util.List;
 @AllArgsConstructor
 public class King extends Piece {
 
-    private final int RANGE = 1;
+    private static final int RANGE = 1;
 
     public King(Coordinates currentPos, Color color, PieceValue value, boolean isActive) {
         super(currentPos, color, value, isActive);
@@ -20,8 +22,8 @@ public class King extends Piece {
     @Override
     public List<Field> findPossibleTakes(Board board) {
         List<Field> possibleTakes = board.getBoardAsList();
-        return possibleTakes.stream().filter(field -> (StreamUtils.isInRange(field.getCoords(), getCurrentPos(), this.RANGE)
-                        && field.getPiece() == null))
+        return possibleTakes.stream().filter(field -> (StreamUtils.isInRange(field.getCoords(), getCurrentPos(), RANGE)
+                        && field.getPiece().getColor() != this.getColor()))
                 .toList();
     }
 }
--- src/main/java/utils/BoardFactory.java
+++ src/main/java/utils/BoardFactory.java
@@ -4,11 +4,13 @@ import model.Board;
 
 public class BoardFactory {
 
-    private final static int START_INDEX = 0;
-    private final static int END_INDEX = 7;
+    private BoardFactory() {}
+
+    private static final int START_INDEX = 0;
+    private static final int END_INDEX = 7;
     public static Board getBoard(String testPreset){
         Board board = new Board(START_INDEX, END_INDEX);
-        if (testPreset == "emptyBoard"){
+        if ("emptyBoard".equals(testPreset)){
             return board;
         }
         return null;
